b0d98778c19f94ac986f37da12f55783a158e44c
newExpand <magin0auto@gmail.com>
2023-09-06 11:05:21 +0900

    채팅방 생성하기

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -2,6 +2,7 @@ import {
   WebSocketGateway,
   WebSocketServer,
   SubscribeMessage,
+  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
@@ -14,5 +15,20 @@ export class ChatGateway {
     const { message, nickname } = data;
 
     socket.broadcast.emit('message', `${nickname}: ${message}`);
+  }
+}
+
+@WebSocketGateway({ namespace: 'room' })
+export class RoomGateway {
+  rooms = [];
+
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('createRoom')
+  handleMessage(@MessageBody() data) {
+    const { nickname, room } = data;
+    this.rooms.push(room);
+    this.server.emit('rooms', this.rooms);
   }
 }
--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 const socket = io('http://localhost:3000/chat');
+const roomSocket = io('http://localhost:3000/room');
 const nickname = prompt('닉네임을 입력해 주세요');
 
 socket.on('connect', () => {
@@ -13,4 +14,19 @@ function sendMessage() {
 
 socket.on('message', (message) => {
   $('#chat').append(`<p>${message}</p>`);
+});
+
+function createRoom() {
+  const room = prompt('생성할 방의 이름을 입력해주세요.');
+  roomSocket.emit('createRoom', { room, nickname });
+}
+
+roomSocket.on('rooms', (data) => {
+  console.log(data);
+  $('#rooms').empty();
+  data.forEach((room) => {
+    $('#rooms').append(
+      `<li>${room} <button onclick="joinRoom('${room}')">참가</button></li>`,
+    );
+  });
 });
