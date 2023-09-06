14b57ee1cc5f096e23a1e619b707d34622258a65
newExpand <magin0auto@gmail.com>
2023-09-06 09:17:11 +0900

    서버 측 작업을 위한 게이트웨이 만들기 및 모듈에 등록

---
+++ src/app.gateway.ts
@@ -0,0 +1,19 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(socket: Socket, data: any): void {
+    this.server.emit(
+      'message',
+      `client-${socket.id.substring(0, 4)} : ${data}`,
+    );
+  }
+}
